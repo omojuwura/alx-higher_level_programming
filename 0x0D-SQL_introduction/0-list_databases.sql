afce30608c8aa2cf78eab46968294b8027ae4b65
omojuwura <omowumijuwura@gmail.com>
2023-02-16 14:34:28 -0800

    push

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,2 @@
--- a script that lists all databases of your MySQL server
+--script that lists all databases of mysql server
 SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,1 @@
+--Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
