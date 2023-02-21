3b1f4531ab82ac9cc856e7e64cf573a4f2a316dd
omojuwura <omowumijuwura@gmail.com>
2023-02-21 06:21:52 -0800

    sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- a list that lists the records of the second Table
+SELECT score, name FROM second_table
+WHERE name != ''
+ORDER BY score DESC;
